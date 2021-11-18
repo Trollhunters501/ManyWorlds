e20ffbe35429b3ace8b690402840f4d02f781f3a
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2021-11-18 13:28:57 -0600

    Update Main.php
    Mw lvdat command removed

--- src/aliuly/manyworlds/Main.php
+++ src/aliuly/manyworlds/Main.php
@@ -27,7 +27,6 @@ class Main extends BasicPlugin implements CommandExecutor{
 			"MwCreate",
 			"MwGenLst",
 			"MwLoader",
-			"MwLvDat",
 			"MwDefault",
 		] as $mod){
 			$mod = __NAMESPACE__ . "\\" . $mod;
